97f93a72c5cdca0ebbd21cd849b983cb75223b40
Maxime Alay-Eddine <maxime.alayeddine@gmail.com>
2015-01-09 22:53:50 +0100

    Update CGE.rb

--- CGE.rb
+++ CGE.rb
@@ -21,14 +21,24 @@ browser = Watir::Browser.new
   if browser.text.include? "Page non trouvée"
     next
   else
-    nom_ecole = browser.div(:id => "fiche-ecole").div(:class => "right-column").h2.text
-    url = browser.div(:id => "fiche-ecole").div(:class => "right-column").h3.text
-    email = String.new(url)
-    email = email.split("/")[0]
-    email_tab = email.split(".")
-    email_tab.shift
-    email = "bde@"+email_tab.join(".")
-    puts nom_ecole+";;"+url+";;"+email
+    nom_ecole_object = browser.div(:id => "fiche-ecole").div(:class => "right-column").h2
+    if nom_ecole_object.exists?
+      nom_ecole = nom_ecole_object.text
+      
+      url_object = browser.div(:id => "fiche-ecole").div(:class => "right-column").h3
+      if url_object.exists?
+        url = url_object.text
+        
+        #Ici, manipulez les données
+        email = String.new(url)
+        email = email.split("/")[0]
+        email_tab = email.split(".")
+        email_tab.shift
+        email = "bde@"+email_tab.join(".")
+        puts nom_ecole+";;"+url+";;"+email
+        
+      end
+    end
   end
 end
 
